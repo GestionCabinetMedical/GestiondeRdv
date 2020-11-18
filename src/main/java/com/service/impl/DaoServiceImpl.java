fc1058bbde967e8dd7df4e8355f58f995c92af5d
Maxime Rembert <maxime.rembert@gmail.com>
2020-11-18 15:43:28 +0100

    implementation methode CRUD

--- src/main/java/com/service/impl/DaoServiceImpl.java
+++ src/main/java/com/service/impl/DaoServiceImpl.java
@@ -3,10 +3,42 @@
  */
 package com.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.repo.IDaoRepo;
+import com.service.IDaoService;
+
 /**
+ * Classe service générique implémentant IDaoService.
+ * 
  * @author Maxime
  *
  */
-public class DaoServiceImpl {
+public class DaoServiceImpl<E> implements IDaoService<E>{
+	
+	@Autowired
+	private IDaoRepo<E> repo;
+
+	@Override
+	public E addOrUpdate(E entite) {
+		return repo.save(entite);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		repo.deleteById(id);
+	}
+
+	@Override
+	public E findById(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public List<E> findAll() {
+		return repo.findAll();
+	}
 
 }
